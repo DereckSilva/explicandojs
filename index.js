f41ab02e4d09d1b4396e43dd9121911de6160d8b
Dereck Vinicius Silva <viniciusdereck39@gmail.com>
2021-03-04 11:02:59 -0300

    código alterado

---
+++ index.js
@@ -0,0 +1,33 @@
+// Explicando as declarações de variáveis com var e let
+
+
+// para declarar uma variavel com let e var eu escrevo com qual usar
+// depois atribuo um nome de uma variavel 
+// logo depois atribuo um valor a ela
+var nome = "Dereck"; 
+var numero = 21;
+console.log(nome, numero);
+console.log(); // pular uma linha
+
+let nome1 = "Dereck";
+let numero1 = 21;
+console.log(nome1, numero1);
+console.log();
+
+
+// a forma de declarar ambas variáveis são iguais, porém tem uma diferença dentro das duas
+
+var nomeAluno = "Andre";
+var nomeAluno = "Lucas";
+console.log(nomeAluno);
+console.log();
+
+// com var eu consigo alterar o valor de uma variável
+// quando esse código rodas, não irá gerar erro
+
+let nomeAlunoNovo = "João";
+//let nomeAlunoNovo = "Mateus";
+console.log(nomeAlunoNovo);
+
+// com let eu não consigo alterar o valor de uma variavel
+// esse código com let irá gerar um erro
